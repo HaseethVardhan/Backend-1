ee37cc8a2962923b31309d8a5f03959c47d22d35
HaseethVardhan <haseethvardhan01@gmail.com>
2025-01-01 17:21:36 +0530

    Add additional user routes for token refresh, password change, account updates, and profile retrieval

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express'
-import {registerUser, loginUser, logoutUser} from '../controllers/register.controller.js'
+import {registerUser, loginUser, logoutUser, refreshAccessToken, changePassword, getCurrentUser, changeAccountDetails, updateAvatar, updateCoverImage, getUserChannelProfile, getWatchHistory} from '../controllers/register.controller.js'
 import {upload} from '../middlewares/multer.middleware.js'
 import {verifyJWT} from '../middlewares/auth.middleware.js'
 
@@ -20,5 +20,16 @@ router.route('/register').post(
 
 router.route('/login').post(loginUser)
 router.route('/logout').post(verifyJWT, logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changePassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, changeAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateCoverImage)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
+
 
 export default router
